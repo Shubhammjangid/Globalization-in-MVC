751035cf34e7bcff71a8d8454b4d522aec0547e9
Shubhammjangid <shubham.jangid@netsolutions.com>
2022-02-04 15:30:47 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Globalization.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Localization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globalization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHtmlLocalizer<HomeController> localizer;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger , IHtmlLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            this.localizer= localizer;
+        }
+
+        public IActionResult Index()
+        {
+            var test = localizer["Hello World"];
+            ViewData["Hello World"] = test;
+            return View();
+        }
+        public IActionResult Info()
+        {
+            var Example = localizer["Employee Name"];
+            ViewData["Employee Name"] = Example;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Globalization.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace Globalization.Models
+{
+    public class Employee
+    {
+        
+
+        //[DisplayName("Employee Name")]
+        [Required(ErrorMessage ="Enter your name")]
+       
+        public string Name { get; set; }
+
+        //[DisplayName("Employee City Name")]
+        [Required(ErrorMessage = "Enter your City name")]
+        
+        public string CityName { get; set; }
+    }
+}
